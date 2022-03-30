e13bf17f462cfbd86387200ac7cfcbba548e3342
Sagun Rajput <71062659+Sagun-rajput@users.noreply.github.com>
2022-03-30 20:18:23 +0530

    Time: 177 ms (5.02%), Space: 19.9 MB (5.16%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -9,21 +9,17 @@ public:
                 int tempAns = minFallingSum(matrix, 0, j, row, col, m);
                 ans = min(ans, tempAns);
             }
-        return ans;
-}
-        
+        return ans;   }      
 private: 
         int minFallingSum(vector<vector<int>>& matrix,int currentRow, int currentCol, int row, int col, unordered_map<string, int>&m){
             if(currentCol < 0 || currentCol >= col)
-                return 1000001;
+                return 100001;
             
             if(currentRow == row-1)
-                return matrix[currentRow][currentCol];
-            
+                return matrix[currentRow][currentCol];            
     string key = to_string(currentRow) + "_" + to_string(currentCol);
     if(m.find(key) != m.end())
-        return m[key];
-            
+        return m[key];         
     int leftDiagonal = matrix[currentRow][currentCol] + minFallingSum(matrix, currentRow + 1, currentCol-1, row, col, m);
     int down = matrix[currentRow][currentCol] + minFallingSum(matrix, currentRow + 1, currentCol, row, col, m);
     int rightDiagonal = matrix[currentRow][currentCol] + minFallingSum(matrix, currentRow + 1, currentCol+1, row, col, m);
