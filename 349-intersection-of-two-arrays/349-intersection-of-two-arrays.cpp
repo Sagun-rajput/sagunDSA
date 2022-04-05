eb3eab0daa928c80eabe1b344ac4927114eeb687
Sagun Rajput <71062659+Sagun-rajput@users.noreply.github.com>
2022-04-05 16:00:39 +0530

    Time: 7 ms (69.09%), Space: 10.4 MB (53.67%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -2,9 +2,12 @@ class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         unordered_map<int, bool>m;
+        // traversing nums1 and entering values in map m
         for(auto i : nums1)
             m[i] = true;
-        vector<int> arr;
+        // initialisng a new array
+        vector<int> arr; 
+         // traversing through nums2 and matching the value
         for(auto j:nums2){
             if(m.find(j) != m.end() &&  m[j]){
                 arr.push_back(j);
