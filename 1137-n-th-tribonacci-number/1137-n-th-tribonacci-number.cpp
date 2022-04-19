0bc49666b0ff9a086ac6ef56370ed3b013f6c18b
Sagun Rajput <71062659+Sagun-rajput@users.noreply.github.com>
2022-04-19 22:43:49 +0530

    Time: 2 ms (32.81%), Space: 6.2 MB (6.09%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int tribonacci(int n) {
-        unordered_map<int, int> m;
+        unordered_map<int,int> m;
         return trib(n, m);
     }
-    
-private: 
-    int trib(int n, unordered_map<int, int>&m){
+private:
+    int trib(int n, unordered_map<int,int>&m){
         if(n == 0) return 0;
         if(n == 1 || n == 2) return 1;
-        
         int key = n;
         if(m.find(key) != m.end())
             return m[key];
+        int tri = trib(n-1, m) + trib(n-2, m) + trib(n-3, m);
         
-        m[key] = trib(n-1, m) + trib(n-2, m) + trib(n-3, m);;
+        m[key] = tri;
         return m[key];
+            
     }
 };
