304cd3e7c551665ff376a25b5b1ee062e63e49e4
Sagun Rajput <71062659+Sagun-rajput@users.noreply.github.com>
2022-04-19 22:26:28 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (10.40%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     int fib(int n) {
-        return fibonacci(n);
+        unordered_map<int,int> m;
+        return fibonacci(n, m);
     }
     
 private:
-   int fibonacci(int n){
+   int fibonacci(int n, unordered_map<int,int>&m){
         if(n == 0 || n == 1)
             return n;
+        int key = n;
+       if(m.find(key) != m.end())
+           return m[key];
+        int fib = fibonacci(n-1, m) +  fibonacci(n-2, m);
         
-        int fib = fibonacci(n-1) +  fibonacci(n-2);
-        
-        return fib;
+        m[key] = fib;
+        return m[key];
     }
 };
