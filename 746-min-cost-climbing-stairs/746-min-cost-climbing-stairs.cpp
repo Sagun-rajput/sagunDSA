3cddf905684946230ba0b17aad6273c23019c443
Sagun Rajput <71062659+Sagun-rajput@users.noreply.github.com>
2022-04-27 11:49:07 +0530

    Time: 23 ms (7.11%), Space: 15.5 MB (7.51%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-        unordered_map<int,int> m;
-        return min(minCost(0, cost, m), minCost(1, cost, m));
+      unordered_map<int, int> m;
+      return min(minCost(0, cost, m), minCost(1, cost, m));
     }
 private:
-    int minCost(int CI, vector<int>&cost, unordered_map<int,int>&m){
-        if(CI >= cost.size()) return 0;
-        
+    int minCost(int CI, vector<int>& cost, unordered_map<int, int>&m){
+        if(CI >= cost.size())
+            return 0;
         int key = CI;
-        if(m.find(key) != m.end())
+        if(m.find(key) !=m.end())
             return m[key];
         int oneStep = cost[CI] + minCost(CI+1, cost, m);
         int twoSteps = cost[CI] + minCost(CI+2, cost, m);
