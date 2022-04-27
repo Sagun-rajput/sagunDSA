ee23e3dd91fbbdb509d6a6d4996cc85ecdbde24e
Sagun Rajput <71062659+Sagun-rajput@users.noreply.github.com>
2022-04-27 12:40:22 +0530

    Time: 5 ms (17.34%), Space: 8.3 MB (5.48%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -2,22 +2,19 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         unordered_map<int, int> m;
-        return robber(nums, 0, m);
+        return houseRobber(0, nums, m);
     }
-    
 private:
-    int robber(vector<int>& nums, int CI, unordered_map<int, int>& m){
-        if(CI>= nums.size())
+    int houseRobber(int CH, vector<int>&nums, unordered_map<int, int>&m){
+        if(CH >= nums.size())
             return 0;
-        
-        int key = CI;
+        int key = CH;
         if(m.find(key) != m.end())
             return m[key];
+        int rob = nums[CH] + houseRobber(CH+2, nums, m);
+        int dontRob = houseRobber(CH+1, nums, m);
         
-        int rob = nums[CI] + robber(nums, CI+2, m);
-        int noRob = robber(nums, CI+1, m);
-        
-        m[key] = max(rob, noRob);
-        return m[key];
+        m[key] = max(rob, dontRob);
+        return m[key]; 
     }
 };
