16b0de7c90700cde952bccc51d670ae6b8a3ccbd
Sagun Rajput <71062659+Sagun-rajput@users.noreply.github.com>
2022-04-28 22:57:20 +0530

    Time: 247 ms (49.07%), Space: 39.5 MB (29.11%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,38 +1,27 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        int sum =0;
-        for(auto i:nums)
-            sum += i;
-        if(sum %2 != 0)
+        int sum = 0;
+        unordered_map<string, int>m;
+        for(auto i : nums)
+            sum+=i;
+        if(sum%2 != 0)
             return false;
-        unordered_map<string, int> m;
-        return equalSubsetSum(nums, 0, sum/2, m);
+        return answer(0, sum/2, nums, m);
     }
 private:
-    bool equalSubsetSum(vector<int>&nums, int CI, int total, unordered_map<string, int>& m){
-        if(CI >= nums.size())
-            return false;
-        if(total == 0)
-            return true;
-        
-        bool consider = false;
-        string key = to_string(CI) + "_" + to_string(total);
+    bool answer(int CI, int W, vector<int>&nums, unordered_map<string, int> &m){
+        if(CI>=nums.size()) return false;
+        if(W == 0) return true;
+        bool canPick = false;
+        string key = to_string(CI) + "_" + to_string(W);
         if(m.find(key) != m.end())
             return m[key];
-        
-        if(total >= nums[CI])
-            consider = equalSubsetSum(nums, CI + 1, total - nums[CI], m);
-            
-        if(consider){
-            m[key];
-            return true;
-        }
-        bool notConsider = equalSubsetSum(nums, CI + 1, total, m);
-            
-        m[key] = (consider || notConsider);
+        if(W >= nums[CI])
+            canPick = answer(CI+1, W-nums[CI], nums, m);
+        if(canPick) return true;
+        bool cantPick = answer(CI+1, W, nums, m);
+        m[key] = (canPick || cantPick);
         return m[key];
-            
-        
     }
 };
